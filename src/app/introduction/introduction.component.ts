a2daac9a90ade511abc77070700fc79b4799e1fb
陳鈞 <jim60105@gmail.com>
2021-03-28 00:42:18 +0800

    調整intro圖片

--- src/app/introduction/introduction.component.ts
+++ src/app/introduction/introduction.component.ts
@@ -6,7 +6,21 @@ import { Component, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['./introduction.component.scss']
 })
 export class IntroductionComponent implements OnInit {
-  @Input() public imgSrc = '';
+  // tslint:disable-next-line: variable-name
+  private _imgSrc = '';
+  private initTime = Date.now();
+
+  get imgSrc(): string {
+    let url = this._imgSrc;
+    if (url.includes('file.core.windows.net')) {
+      url += '&timestamp=' + this.initTime;
+    }
+    return url;
+  }
+  @Input() set imgSrc(v: string) {
+    this._imgSrc = v;
+  }
+
   @Input() public intro = '';
 
   constructor() {
