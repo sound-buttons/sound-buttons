99b2a7350935334fbdb1886a9741e9c32047135b
陳鈞 <jim60105@gmail.com>
2021-03-30 14:56:29 +0800

    修正非表列語系即不顯示問題
    未成功比對時就使用第一個語言

--- src/app/language.service.ts
+++ src/app/language.service.ts
@@ -7,8 +7,13 @@ export class LanguageService {
   static BrowserLanguage = () => navigator.language || (navigator.languages || ['en'])[0];
   static GetTextFromObject(obj: any = {}): string {
     // tslint:disable-next-line: variable-name
-    const _text = obj[this.BrowserLanguage().toLowerCase()] as string;
-    return _text ? _text : '';
+    let _text;
+    Object.keys(obj).forEach(key => {
+      if (this.BrowserLanguage().toLowerCase().includes(key)) {
+        _text = obj[key] as string;
+      }
+    });
+    return _text ? _text : obj[Object.keys(obj)[0]];
   }
 
   constructor() { }
