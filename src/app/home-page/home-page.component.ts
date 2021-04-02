2d0af96fe22d8c638984a7107c00ef6e328bf43d
陳鈞 <jim60105@gmail.com>
2021-04-02 17:06:02 +0800

    初版Home Page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -9,14 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
   public configs$!: Observable<IFullConfig[]>;
-  private url = 'assets/configs/main.json';
 
   constructor(
     private configService: ConfigService
   ) { }
 
   ngOnInit(): void {
-    this.configs$ = this.configService.getBriefConfig(this.url);
+    this.configs$ = this.configService.getBriefConfig();
   }
 }
 
