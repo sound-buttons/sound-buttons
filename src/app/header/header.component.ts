a0b112076bd01a2c4dbbe1029f1bf453f86b9a0c
陳鈞 <jim60105@gmail.com>
2021-04-11 16:55:15 +0800

    在header component增加路由按鈕

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,3 +1,5 @@
+import { ConfigService } from './../services/config.service';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   title = 'Sound Buttons';
-  constructor() { }
+  name = '';
+  constructor(
+    private configService: ConfigService,
+    public router: Router) { }
 
   ngOnInit(): void {
+    this.configService.OnConfigChanged.subscribe(c => {
+      this.name = c.name;
+    });
   }
 
 }
