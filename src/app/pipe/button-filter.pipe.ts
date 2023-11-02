a33ea5c69903b2deb7406dc02d939e47a054f57a
陳鈞 <jim60105@gmail.com>
2023-11-02 20:25:59 +0800

    refactor: refactor ButtonFilterPipe to avoid search twice issue
    - Add import for `ButtonGroup` at the top of the file
    - Refactor `transform` method to use `map` instead of `forEach`
    
    Signed-off-by: 陳鈞 <jim60105@gmail.com>

--- src/app/pipe/button-filter.pipe.ts
+++ src/app/pipe/button-filter.pipe.ts
@@ -1,14 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { IButtonGroup } from './../sound-buttons/ButtonGroup';
+import { ButtonGroup, IButtonGroup } from './../sound-buttons/ButtonGroup';
 
 @Pipe({
   name: 'buttonFilter',
 })
 export class ButtonFilterPipe implements PipeTransform {
   transform(items: IButtonGroup[], term: string): IButtonGroup[] {
-    (items as IButtonGroup[]).forEach((group) => {
-      group.buttons = group.buttons.filter((button) => button.text.indexOf(term) !== -1);
-    });
-    return items;
+    return (items as IButtonGroup[]).map(
+      (group) =>
+        new ButtonGroup(
+          group.name,
+          group.baseRoute,
+          group.buttons.filter((button) => button.text.indexOf(term) !== -1)
+        )
+    );
   }
 }
