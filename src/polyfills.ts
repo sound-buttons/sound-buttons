4eff3637367ed1008ef661fd8b2d8ce0d86398c0
陳鈞 <jim60105@gmail.com>
2023-11-26 02:12:10 +0800

    Revert "fix: [Violation] Added non-passive event listener to a scroll-blocking <some> event."
    This reverts commit ed18fdf96852ce3e8b983db9fec82ee1af5f9e56.
    
    This solution breaks the number input scrolling at upload page.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -42,6 +42,7 @@
  *
  */
 
+import './zone-flags';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
---
+++ src/zone-flags.ts
@@ -0,0 +1,2 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+(window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove', 'touchstart']; // disable patch specified eventNames
